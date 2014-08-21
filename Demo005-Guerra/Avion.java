3a11681f9477c3649d81083ddc19da648d451c05
facu95 <facu_095@hotmail.com>
2014-08-21 15:18:02 -0300

    Update Avion.java

--- Demo005-Guerra/Avion.java
+++ Demo005-Guerra/Avion.java
@@ -11,14 +11,26 @@ public class Avion extends Participante
         //Ejemplo 1
         //enemigo.danio(400);
         
-        //Ejemplo 2
-        if(enemigo instanceof Tanque){
-            enemigo.danio(600);
-        }else if(enemigo instanceof Avion){
-            enemigo.danio(500);
-        }else if(enemigo instanceof Soldado){
-            enemigo.danio(200);
+//         //Ejemplo 2
+//         if(enemigo instanceof Tanque){
+//             enemigo.danio(600);
+//         }else if(enemigo instanceof Avion){
+//             enemigo.danio(500);
+//         }else if(enemigo instanceof Soldado){
+//             enemigo.danio(200);
+        enemigo.danio(enemigo.danioParaAvion());
         }
-    }
-    
-}
+        
+        public double danioParaTanque(){
+                return 700*usarEscudo();
+            }
+        public double danioParaSoldado(){
+                return 350*usarEscudo();
+            }        
+        public double danioParaAvion(){
+                return 500*usarEscudo();
+            }        
+  
+
+        
+        }
